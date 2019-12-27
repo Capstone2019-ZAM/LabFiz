867e2167175dfb70bb45e14cf92b24aa5b5af55e
0x5A61696E <55599368+0x5A61696E@users.noreply.github.com>
2019-12-26 18:30:56 -0700

    model seeders
    -added model seeders for report models and submodels

---
+++ app/database/seeds/ReportSectionsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ReportSectionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
