6d1d6fe80abcaabcc30579982785c86cdcfb3b11
0x5A61696E <55599368+0x5A61696E@users.noreply.github.com>
2019-12-27 17:45:03 -0700

    controller repositories added
    - issue controller refactored using repository pattern
    - code cleanup

--- app/app/Http/Controllers/Api/v1/IssueController.php
+++ app/app/Http/Controllers/Api/v1/IssueController.php
@@ -4,77 +4,89 @@ namespace App\Http\Controllers\api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Issue;
+use App\Repositories\ModelRepository;
 use App\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class IssueController extends Controller
 {
-    public function __construct()
-    {
+    protected $model_issue;
+    protected $model_user;
 
+    public function __construct(Issue $issue, User $user)
+    {
+        $this->model_issue = new ModelRepository($issue);
+        $this->model_user = new ModelRepository($user);
     }
 
     public function get($id)
     {
-        $result = ['status' => '400 (Bad Request)', 'message' => 'Ill formed input', 'data' => ''];
-        $report = Issue::where('id', $id)->first();
-        if (!$report) {
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+
+        try {
+            $issue = $this->model_issue->getById($id);
+            $result['data'] = $issue;
+        } catch (Exception $ex) {
+            $result['message'] = $ex->getMessage();
             return response($result, 400);
         }
+
         $result['status'] = '200 (Ok)';
         $result['message'] = 'Issue retrieved succesfully.';
-        $result['data'] = $report;
         return response($result, 200);
     }
 
     public function get_all()
     {
-        $result = ['status' => '400 (Bad Request)', 'message' => 'Ill formed input', 'data' => ''];
-        $report = Issue::all();
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+        $result['data'] = $this->model_issue->get();
         $result['status'] = '200 (Ok)';
         $result['message'] = 'All Issues retrieved succesfully.';
-        $result['data'] = $report;
         return response($result, 200);
     }
 
     public function create(Request $request)
     {
-        $result = ['status' => '400 (Bad Request)', 'message' => 'Ill formed input', 'data' => []];
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => []];
         $header = $request->header('Authorization');
-        $user = User::where('api_token', $header)->first();
+        $user = $this->model_user->getByColumn($header, 'api_token');
 
-        // create the inspection assignment
-        $issue = new Issue();
-        $issue->title = $request->title;
-        $issue->room = $request->room;
-        $issue->user_id = $user->id;
-        $issue->severity= $request->severity;
-        $issue->status = 'incomplete';
-        $issue->description = $request->description;
-        $issue->comments = $request->comments;
-        if(!$issue->save()){
-            $result['message'] = 'Failed to persist changes.';
+        try {
+            $result['data'] = $this->model_issue->create(
+                [
+                    'title' => $request->title,
+                    'room' => $request->room,
+                    'user_id' => $user->id,
+                    'severity' => $request->severity,
+                    'status' => 'incomplete',
+                    'description' => $request->description,
+                    'comments' => $request->comments
+                ]
+            );
+        } catch (Exception $ex) {
+            $result['message'] = $ex;
             return response($result, 400);
         }
 
         $result['status'] = '200 (Ok)';
-        $result['data'] = $issue;
         $result['message'] = 'Created issue succesfully!';
         return response($result, 200);
     }
 
     public function delete($id)
     {
-        $result = ['status' => '400 (Bad Request)', 'message' => 'Could not find issue to be deleted by id', 'data' => ''];
-        $issue = Issue::where('id', $id)->first();
-        if (!$issue) {
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+
+        try {
+            $result['data'] = $this->model_issue->deleteById($id);
+        } catch (Exception $ex) {
+            $result['message'] = $ex->getMessage();
             return response($result, 400);
         }
 
-        $issue->delete();
         $result['status'] = '200 (Ok)';
-        $result['message'] = 'Issue deleted succesfully.';
-        $result['data'] = $issue;
+        $result['message'] = 'Issue deleted succesfully';
         return response($result, 200);
     }
 }
---
+++ app/app/Repositories/ModelRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+use JasonGuru\LaravelMakeRepository\Repository\BaseRepository;
+
+/**
+ * Class ModelRepository.
+ */
+class ModelRepository extends BaseRepository
+{
+    protected $model;
+
+    /**
+     * ModelRepository constructor.
+     * dependancy injection for eloquent models
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @return string
+     *  Return the model
+     */
+    public function model()
+    {
+        return $this->model;
+    }
+}
