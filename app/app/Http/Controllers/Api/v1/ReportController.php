513285390a799a03e75bb98acc3171113d2a7a20
0x5A61696E <55599368+0x5A61696E@users.noreply.github.com>
2019-12-27 18:35:03 -0700

    refactored report model and controller with reposiotires
    - refactored reports with reposiotries

--- app/app/Http/Controllers/Api/v1/ReportController.php
+++ app/app/Http/Controllers/Api/v1/ReportController.php
@@ -6,79 +6,109 @@ use App\Http\Controllers\Controller;
 use App\Report;
 use App\ReportQuestion;
 use App\ReportSection;
+use App\Repositories\ModelRepository;
 use App\User;
+use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class ReportController extends Controller
 {
-    public function __construct()
-    {
+    protected $model_report;
+    protected $model_report_section;
+    protected $model_report_question;
+    protected $model_user;
 
+    public function __construct(Report $report, ReportQuestion $report_question, ReportSection $report_section, User $user)
+    {
+        $this->model_report = new ModelRepository($report);
+        $this->model_report_section = new ModelRepository($report_section);
+        $this->model_report_question = new ModelRepository($report_question);
+        $this->model_user = new ModelRepository($user);
     }
 
     public function get($id)
     {
-        $result = ['status' => '400 (Bad Request)', 'message' => 'Ill formed input', 'data' => ''];
-        $report = Report::where('id', $id)->first();
-        if (!$report) {
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+
+        try {
+            $issue = $this->model_report->getById($id);
+
+        } catch (Exception $ex) {
+            $result['message'] = $ex->getMessage();
             return response($result, 400);
         }
+
+        $result['data'] = $issue;
         $result['status'] = '200 (Ok)';
         $result['message'] = 'Report retrieved succesfully.';
-        $result['data'] = $report;
         return response($result, 200);
     }
 
     public function get_all()
     {
-        $result = ['status' => '400 (Bad Request)', 'message' => 'Ill formed input', 'data' => ''];
-        $report = Report::all();
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+        $result['data'] = $this->model_report->get();
         $result['status'] = '200 (Ok)';
         $result['message'] = 'All Reports retrieved succesfully.';
-        $result['data'] = $report;
         return response($result, 200);
     }
 
     public function create(Request $request)
     {
-        $result = ['status' => '400 (Bad Request)', 'message' => 'Ill formed input', 'data' => []];
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => []];
         $header = $request->header('Authorization');
-        $user = User::where('api_token', $header)->first();
-
+        $user = $this->model_user->getByColumn($header, 'api_token');
         $sections = $request->sections;
         $title = $request->title;
 
         // create the report
-        $report = new Report();
-        $report->title = $title;
-        $report->user_id = $user->id;
-        if (!$report->save()) {
-            $result['message'] = 'Invalid input provided for section:' . $title;
+        try {
+            $report = $this->model_report->create(
+                [
+                    'title' => $title,
+                    'user_id' => $user->id,
+                ]
+            );
+        } catch (Exception $ex) {
+            $result['message'] = $ex->getMessage();
             return response($result, 400);
         }
 
-        $result['data'][$title] = $report;
+        $result['data'] = $report;
 
-        // create sections and bind questions
+        // if the report has sections, populate the tables for sections and questions
         if ($sections) {
             foreach ($sections as $sect => $value) {
-                $section = new ReportSection();
-                $section->title = $sect;
-                $section->report_id = $report->id;
-                if (!$section->save()) {
-                    $result['message'] = 'Invalid input provided for section:' . $sect;
+                // create the section
+                try {
+                    $section = $this->model_report_section->create(
+                        [
+                            'title' => $sect,
+                            'report_id' => $report->id,
+                            'user_id' => $user->id,
+                        ]
+                    );
+                } catch (Exception $ex) {
+                    $result['message'] = $ex->getMessage();
                     return response($result, 400);
                 }
+
                 $result['data']['$sect'] = $section;
+
+                // create any questions
                 foreach ($value['qs'] as $q) {
-                    $question = new ReportQuestion();
-                    $question->question = $q;
-                    $question->report_section_id = $section->id;
-                    if (!$question->save()) {
-                        $result['message'] = 'Invalid input provided for question:' . $q;
+                    try {
+                        $question = $this->model_report_question->create(
+                            [
+                                'question' => $q,
+                                'report_section_id' => $section->id,
+                            ]
+                        );
+                    } catch (Exception $ex) {
+                        $result['message'] = $ex->getMessage();
                         return response($result, 400);
                     }
+
                     $result['data'][$q] = $question;
                 }
             }
@@ -91,16 +121,17 @@ class ReportController extends Controller
 
     public function delete($id)
     {
-        $result = ['status' => '400 (Bad Request)', 'message' => 'Could not find report to be deleted by id', 'data' => ''];
-        $report = Report::where('id', $id)->first();
-        if (!$report) {
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+
+        try {
+            $result['data'] = $this->model_report->deleteById($id);
+        } catch (Exception $ex) {
+            $result['message'] = $ex->getMessage();
             return response($result, 400);
         }
 
-        $report->delete();
         $result['status'] = '200 (Ok)';
         $result['message'] = 'Report deleted succesfully.';
-        $result['data'] = $report;
         return response($result, 200);
     }
 }
--- app/app/Report.php
+++ app/app/Report.php
@@ -12,7 +12,7 @@ class Report extends Model
      * @var array
      */
     protected $fillable = [
-        'title'
+        'title', 'user_id'
     ];
 
     public function user(){
