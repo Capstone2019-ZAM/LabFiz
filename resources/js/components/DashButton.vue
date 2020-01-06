a1aed2e3cd943924bd3a89f1915796b765c0c0c5
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-01-05 21:52:39 -0600

    Dashboard initialized

--- resources/js/components/DashButton.vue
+++ resources/js/components/DashButton.vue
@@ -1,30 +1,34 @@
 <template>
-<div class="dashboard_wrap">
-     <v-card class="mx-auto "
-     max-width="400"
-     centered
-     hover="true"
-     outlined="true"
-     wrap
-     >     
-      <v-responsive centered class="" >
-      <v-icon  centered>mdi-account</v-icon>
-      </v-responsive>
-    <v-card-text>
-      <v-card-title class="headline">
-      HELOOW
-    </v-card-title>
-      <v-card-subtitle>
-      Some helper
-      </v-card-subtitle>
-      </v-card-text>
-      </v-card>
-  
-</div>
+    <v-card max-width="420" max-height="200" class="mx-auto pa-3 d-flex flex-row" hover outlined>
+      <v-layout row wrap align-content-center >
+        <v-flex xs2 md4 centered>
+            <v-icon size="100">{{data.icon}}</v-icon>
+          
+        </v-flex>
+
+        <v-flex xs10 md8 >
+          <v-card-text>
+            <v-card-title class="headline">
+              <span >{{data.title}} </span> </v-card-title>
+
+            <v-card-subtitle class="grey--light"> <span>{{data.description}} </span> </v-card-subtitle>
+          </v-card-text>
+        </v-flex>
+      </v-layout>
+    </v-card>
 </template>
 
 <script>
 export default {
-  
-}
+ props: ['data']
+};
 </script>
+
+
+// data() {
+//     return {
+//       icon: 'mdi-file-document-edit',
+//       title: 'Inspections',
+//       description: 'Something',
+//     }
+//  }
--- resources/views/dashboardPage.blade.php
+++ resources/views/dashboardPage.blade.php
@@ -3,13 +3,8 @@
 @section('content')
 
     <div id="app">
-        <v-app>
-            <dash-board></dash-board>
-            {{-- <table-component> </table-component> --}}
-            {{-- <dash-button></dash-button> --}}
-        {{-- <example-component></example-component> --}}
-        {{-- <login-form></login-form> --}}
-    
+        <v-app >
+            <dash-board></dash-board>              
     </v-app>
     </div>
     {{-- <script type="text/javascript" src="js/app.js"></script> --}}
