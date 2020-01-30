13daf7df216b24abdfdb20dd34d0289eedf77b48
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-01-29 21:19:20 -0600

    Update TemplateForm.vue

--- app/resources/js/components/TemplateForm.vue
+++ app/resources/js/components/TemplateForm.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center">
-    <v-card width="80%" max-height="600px" class="mx-auto">
+    <v-card width="80%" class="mx-auto">
       <v-row align="center" justify="center">
         <v-col cols="12" md="9">
           <v-card-title>Template Builder</v-card-title>
@@ -31,30 +31,38 @@
             <v-row >
               <v-col cols="12" md="5">
                 <v-text-field
-                  v-model="section_title"
-                  :counter="10"
+                  v-model="sec.section_title"
+                 
                   label="Section Title"
+                  outlined
                   required
                 ></v-text-field>
               </v-col>
+            <v-col cols="12" md="3">
+               <v-btn  color="danger" @click="removeSection(sec,sections)">
+              <v-icon>mdi-minus-circle</v-icon>Remove Section
+            </v-btn>
+            </v-col>
             </v-row>
-            <v-row>
+            <v-row  v-for="(qs,index) in sec.questions" :key="index">
               <v-col cols="12" md="10">
-                <v-text-field v-model="questions" :rules="qRules" label="Question" required></v-text-field>
+                <v-text-field  v-model="qs[index]" :rules="qRules" label="Question" required ></v-text-field>
               </v-col>
             </v-row>
 
             <v-row align="center" justify="center">
-              <v-btn color="primary">
+              <v-btn color="primary" @click="addQuestion(sec)">
                 <v-icon>mdi-plus-circle</v-icon>Add Question
               </v-btn>
             </v-row>
           </div>
           <v-row class="ma-3"></v-row>
           <v-row align="center" justify="center">
-            <v-btn block color="primary">
+            <v-btn block color="primary" @click="addSection(this, sections)">
               <v-icon>mdi-plus-circle</v-icon>Add Section
             </v-btn>
+            
+           
           </v-row>
         </v-container>
       </v-form>
@@ -65,12 +73,44 @@
 export default {
   data: () => ({
     valid: false,
-    // title: "",
+     title: "",
     // questions: "",
     // section_title: "",
     qRules: [v => !!v || "Question is required"],
-    sections : [ {section_nm : "Section 1", id : 1 , questions: ["How are u?", "How do u do"]}]
-    })
+    sections : [ {section_nm : "Section 1", id : 1 , questions: ["A1w are u?", "B2w do u do"]},
+                  {section_nm : "Section 2", id :2 , questions: [ "H3w do u do"]}]
+    }),
+    methods :{
+
+      addQuestion :function(sec){
+        debugger
+        sec.questions.push("");
+        console.log('Question Added')
+      },
+      removeQuestion : function(sec){
+        debugger
+        sec.questions.pop();
+        
+        console.log('Question removed')
+      },
+      addSection : function(t , sec){
+        var newSec = Object();
+        newSec.section_nm = "";
+        newSec.questions = Array();
+        
+        newSec.id= sec.length
+        sec.push(newSec);
+        console.log('Section Added')
+      },
+      removeSection : function(sec,sections){
+        const toRemove = ( element => element.id = sec.id)
+        sections.splice(sections.findIndex(toRemove) ,1);
+        console.log('Section removed')
+      },
+      
+      
+
+    }
 };
 </script>
 
