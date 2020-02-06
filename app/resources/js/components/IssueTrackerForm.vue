cb1ec3f5df2e42e7c6e7a5b53e9ef76310c40669
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-02-06 01:43:40 -0600

    Update IssueTrackerForm.vue

--- app/resources/js/components/IssueTrackerForm.vue
+++ app/resources/js/components/IssueTrackerForm.vue
@@ -1,61 +1,117 @@
 <template>
-    <div id="app">
-        <v-form>
-            <v-card class="mx-auto" max-width="750" outlined >
-            <v-card-title class="justify-center" >
-                <span class="headline" >Issue Tracker Form</span>
-            </v-card-title>
-            <v-divider></v-divider>
-            <v-card-text>
-                <v-container>
-                <v-row>
-                    <v-col class="d-flex" cols="12" sm="8" md="8">
-                    <v-text-field v-model="Title" placeholder="Title" :rules="titleRules" clearable outlined></v-text-field>
-                    </v-col>
-                    <v-col class="d-flex" cols="12" sm="6" md="4">
-                    <v-select :items="status" label="Status" outlined></v-select>
-                    </v-col>
-                </v-row>
-                <v-row>
-                    <v-col class="d-flex" cols="12" sm="6" md="3">
-                    <v-select :items="lab" label="Lab#" outlined></v-select>
-                    </v-col>
-                    <v-col class="d-flex" cols="12" sm="6" md="5">
-                    <v-select :items="assign" label="Assigned To" outlined></v-select>
-                    </v-col>
-                    <v-col class="d-flex" cols="12" sm="6" md="3">
-                    <v-select :items="severity" label="Severity" outlined></v-select>
-                    </v-col>
-                </v-row>    
-                <v-col cols="12" md="fill">
-                    <v-textarea solo name="input-15-4" :counter="150" label="Description" clearable></v-textarea>
-                 </v-col>
-                 <v-col cols="12" md="fill">
-                    <v-textarea solo name="input-15-4" :counter="100" label="Comments" clearable></v-textarea>
-                 </v-col>
-                 <v-row align="center" justify="center">
-                    <v-btn color="primary">
-                        <v-icon>mdi-mouse</v-icon>Post
-                    </v-btn>
-                </v-row>
-                </v-container>
-            </v-card-text>
-            </v-card>
-        </v-form>
-    </div>
+  <div id="app">
+    <v-form>
+      <v-card class="mx-auto" max-width="750" outlined>
+        <v-card-title class="justify-center">
+          <span class="headline">Issue Tracker Form</span>
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col class="d-flex" cols="12" sm="8" md="8">
+                <v-text-field
+                  v-model="title"
+                  placeholder="Title"
+                  :rules="titleRules"
+                  clearable
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col class="d-flex" cols="12" sm="6" md="4">
+                <v-select :items="statuses" label="Status" outlined v-model="status"></v-select>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col class="d-flex" cols="12" sm="6" md="3">
+                <v-select :items="labs" label="Lab#" outlined v-model="lab"></v-select>
+              </v-col>
+              <v-col class="d-flex" cols="12" sm="6" md="5">
+                <v-select :items="assignables" label="Assigned To" outlined v-model="assigned_to"></v-select>
+              </v-col>
+              <v-col class="d-flex" cols="12" sm="6" md="3">
+                <v-select :items="severities" label="Severity" outlined v-model="severity"></v-select>
+              </v-col>
+            </v-row>
+            <v-col cols="12" md="fill">
+              <v-textarea solo name="input-15-4" :counter="150" label="Description" v-model="desc"></v-textarea>
+            </v-col>
+            <v-col cols="12" md="fill">
+              <v-textarea
+                solo
+                name="input-15-4"
+                :counter="100"
+                label="Comments"
+                clearable
+                v-model="latest_comment"
+              ></v-textarea>
+            </v-col>
+            <v-row align="center" justify="center">
+              <v-btn color="primary">
+                <v-icon>mdi-mouse</v-icon>Post
+              </v-btn>
+            </v-row>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-form>
+  </div>
 </template>
 <script>
 export default {
     data: ()=>({
         valid:false,
+        id : 1,     //get this dynamic or from url
+        title: null,
+        desc: null,
+        status : null,
+        severity: null,
+        lab: null,
+        assigned_to: null,
+        latest_comment :null,
         titleRules: [
-            v=> !!v || "Title is required"
+            v=> !!v || "Title is required"   ],
 
-        ],
-        status:['Status1','Status2','Status3','Status4'],
-        lab:['Lab1','Lab2','Lab3','Lab4'],
-        assign:['Test1','Test2','Test3','Test4'],
-        severity:['1','2','3','4'],
+        statuses:['Status1','Status2','Status3','Status4'],
+        labs:['Lab1','Lab2','Lab3','Lab4'],
+        assignables:['Test1','Test2','Test3','Test4'],
+        severities:['1','2','3','4'],
     })
+    ,
+    methods:{
+        submitIssue(){
+            if(valid){
+                var req = Object;
+                req= Object.assign(req,this.title,this.desc,this.status,this.severity,this.lab,this.assigned_to);
+
+            //     axios.put("/api/v1/issue/"+this.id, {
+            //     // data : req
+            //     headers: { Authorization: this.AuthStr }
+            //     })
+            //     .then(
+            //     response => {
+            //     console.log("fetch done!");
+            //     this.issues = response.data.data;
+            //     },
+            // error => {console.log("fetch failed!");});
+            //     }
+            }
+        }
+    },
+   mounted() {
+    axios
+      .get("/api/v1/issue/"+this.id, {
+        headers: { Authorization: this.AuthStr }
+      })
+      .then(
+        response => {
+          console.log("fetch done!");
+          this.issues = response.data.data;
+        },
+        error => {
+          console.log("fetch failed!");
+        }
+      );
+    }
 };
 </script>
