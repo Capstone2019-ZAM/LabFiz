838e09c4fb4ef0a7959eff07e99db7e79b1fa6d9
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-02-06 01:43:18 -0600

    Added model value holder

--- app/resources/js/components/IssueTracker.vue
+++ app/resources/js/components/IssueTracker.vue
@@ -1,83 +1,73 @@
 <template>
-<v-container>
+  <v-container>
     <v-card>
-       <v-card-title class="justify-center" >
-                <span class="headline" >Issue Tracker Form</span>
-        </v-card-title>
-        <v-divider></v-divider>
-      <v-data-table
-        :headers="headers"
-        :items="issues"
-      >
-      
-      <template v-slot:item.status="{ item }">
-            <v-chip :color="getColor(item.status)" dark>{{ item.status }}</v-chip>
-          
-          </template>
-
+      <v-card-title class="justify-center">
+        <span class="headline">Issue Tracker Form</span>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-data-table :headers="headers" :items="issues">
+        <template v-slot:item.status="{ item }">
+          <v-chip :color="getColor(item.status)" dark>{{ item.status }}</v-chip>
+        </template>
       </v-data-table>
-       <v-btn block outlined >
-          Add an Entry
-          <v-icon>mdi-plus</v-icon>
-        </v-btn>
+      <v-btn block outlined>
+        Add an Entry
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
     </v-card>
-</v-container>
+  </v-container>
 </template>
 
 
 <script>
-  export default {
-    data () {
-      return {
-        headers: [
-          {
-            text: 'Status',
-            align: 'left',
-            sortable: false,
-            value: 'status',
-          },
-          { text: 'Issue', value: "title" },
-          { text: 'Room #', value: 'room' },
-          { text: 'Assigned To', value: 'user_id' },
-          { text: 'Resoluton Date', value: 'due_date' },
-        ],
-        issues: [
-          // {
-          //   name: 'Status1',
-          //   issue: 33,
-          //   room: 'ED 123',
-          //   assigned: 'Edward Livingstone',
-          //   date: "Jan 02, 2020",
-          // }
-        ],
-      }
-    },
-    methods:{
-      getColor(status_name) {
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Status",
+          align: "left",
+          sortable: false,
+          value: "status"
+        },
+        { text: "Issue", value: "title" },
+        { text: "Room #", value: "room" },
+        { text: "Assigned To", value: "user_id" },
+        { text: "Resoluton Date", value: "due_date" }
+      ],
+      issues: [
+        // {
+        //   name: 'Status1',
+        //   issue: 33,
+        //   room: 'ED 123',
+        //   assigned: 'Edward Livingstone',
+        //   date: "Jan 02, 2020",
+        // }
+      ]
+    };
+  },
+  methods: {
+    getColor(status_name) {
       if (status_name == "Open") return "red";
       else if (status_name == "Closed") return "blue";
       else return "grey";
     }
-    },
+  },
 
-     mounted() {
-    this.loading = true;
+  mounted() {
     axios
-      .get("/api/v1/issues" , {
+      .get("/api/v1/issues", {
         headers: { Authorization: this.AuthStr }
       })
       .then(
         response => {
           console.log("fetch done!");
-          
           this.issues = response.data.data;
-          debugger
         },
         error => {
           console.log("fetch failed!");
-       
         }
       );
   }
-  }
+};
 </script>
