73540927589196a1958fc5ccf264350b1a9183cc
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-02-06 21:47:29 -0600

    Added Comments table and endpoint

---
+++ app/app/Http/Controllers/Api/v1/CommentController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Comment\CreateRequest;
+use App\Comment;
+use App\Repositories\ModelRepository;
+use App\User;
+use App\Issue;
+use Exception;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    protected $model_comment;
+    protected $model_user;
+    protected $model_issue;
+
+    public function __construct(Comment $comment, User $user ,$issue)
+    {
+        $this->model_comment = new ModelRepository($comment);
+        $this->model_user = new ModelRepository($user);
+        $this->model_issue = new ModelRepository($issue);
+
+    }
+
+    public function get($id)
+    {
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+
+        try {
+            $comment = $this->model_comment->getById($id);
+            $result['data'] = $comment;
+        } catch (Exception $ex) {
+            $result['message'] = $ex->getMessage();
+            return response($result, 400);
+        }
+
+        $result['status'] = '200 (Ok)';
+        $result['message'] = 'Comment retrieved succesfully.';
+        return response($result, 200);
+    }
+
+    public function get_all($issue_id)
+    {
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+        $result['data'] = $this->model_comment->get();
+        $result['status'] = '200 (Ok)';
+        $result['message'] = 'All Comment retrieved succesfully.';
+        return response($result, 200);
+    }
+
+    public function create(CreateRequest $request)
+    {
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => []];
+        $header = $request->header('Authorization');
+        $user = $this->model_user->getByColumn($header, 'api_token');
+
+        try {
+            $result['data'] = $this->model_comment->create(
+                [
+                    'content' => $request->content,
+                    'user_id' => $user->id,
+                    //'issue_id' =>$issue->id
+                ]
+            );
+        } catch (Exception $ex) {
+            $result['message'] = $ex->getMessage();
+            return response($result, 400);
+        }
+
+        $result['status'] = '200 (Ok)';
+        $result['message'] = 'Created Comment succesfully!';
+        return response($result, 200);
+    }
+
+    public function delete($id)
+    {
+        $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
+
+        try {
+            $result['data'] = $this->model_comment->deleteById($id);
+        } catch (Exception $ex) {
+            $result['message'] = $ex->getMessage();
+            return response($result, 400);
+        }
+
+        $result['status'] = '200 (Ok)';
+        $result['message'] = 'Comment deleted succesfully';
+        return response($result, 200);
+    }
+}
