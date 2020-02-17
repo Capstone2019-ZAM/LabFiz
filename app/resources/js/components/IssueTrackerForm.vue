26262c33e19047812b74d2daeb82dfce6c17a2e6
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-02-16 22:12:44 -0600

    added resolution data field

--- app/resources/js/components/IssueTrackerForm.vue
+++ app/resources/js/components/IssueTrackerForm.vue
@@ -47,6 +47,30 @@
                 ></v-select>
               </v-col>
             </v-row>
+            <v-row cols="12">
+              <v-col md="3">
+                <v-menu
+                  v-model="menu2"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      :rules="dateRules"
+                      v-model="issue.due_date"
+                      readonly
+                      label="Resolution Date"
+                      v-on="on"
+                      outlined
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="issue.due_date" @input="menu2 = false"></v-date-picker>
+                </v-menu>
+              </v-col>
+            </v-row>
             <v-row cols="12" md="12">
               <v-col>
                 <v-textarea
@@ -130,6 +154,9 @@
   </div>
 </template>
 <script>
+import format from "date-fns/format";
+import parseISO from "date-fns/parseISO";
+
 export default {
   data: () => ({
     AuthStr: localStorage.getItem("api"),
@@ -139,13 +166,15 @@ export default {
     SaveSucc: false,
     loading: false,
     valid: true,
+    menu2: false,
     issue: {
       title: null,
       description: null,
       status: null,
       severity: null,
       room: null,
-      assigned_to: null
+      assigned_to: null,
+      due_date: new Date().toISOString().substr(0, 10)
     },
     latest_comment: null,
     comments: [
@@ -159,6 +188,7 @@ export default {
       {
         id: 2,
         issue_id: 1,
+        user_name: "",
         user_id: 1,
         updated_at: "2020-02-01",
         content: "My comment 2is here"
@@ -168,6 +198,7 @@ export default {
     titleRules: [v => !!v || "Title is required"],
     labRules: [v => !!v || "Lab is required"],
     assignRules: [v => !!v || "Assignee is required"],
+    dateRules: [v => !!v || "Resolution date is required"],
     statusRules: [v => !!v || "Status is required"],
     descRules: [v => !!v || "Description is required"],
     sevRules: [v => !!v || "Severity classification is required"],
@@ -182,8 +213,18 @@ export default {
       "Non-critical"
     ]
   }),
-  computed: {},
+  computed: {
+    formattedDate() {
+      return this.issue.due_date
+        ? format(parseISO(this.issue.due_date), "Do MMM yyyy")
+        : "";
+    }
+  },
   methods: {
+    getNamebyId(t_id){
+     let n= this.assignables.find( x => x.id ==t_id);
+     return n.first_name +" " +n.last_name
+    },
     getFullName(el) {
       return el.first_name + " " + el.last_name;
     },
@@ -192,39 +233,49 @@ export default {
 
       if (this.valid) {
         var req = Object;
-       // req = Object.assign(req, this.id, this.issue);
-          req= this.issue;
+        // req = Object.assign(req, this.id, this.issue);
+        req = this.issue;
         //Save Exisiting Issue
         if (
           Number.isInteger(parseInt(window.location.pathname.split("/").pop()))
         ) {
-          //     axios.post("/api/v1/issue/"+this.id, {
-          //     // data : req
-          //     headers: { Authorization: this.AuthStr }
-          //     })
-          //     .then(
-          //     response => {
-          //     console.log("fetch done!");
-          //     this.issues = response.data.data;
-          //     },
-          // error => {console.log("fetch failed!");});
-          //     }
+          axios
+            .post("/api/v1/issue/" + this.id, req, {
+              headers: {
+                Authorization: this.AuthStr,
+                "Content-Type": "application/json"
+              }
+            })
+            .then(
+              response => {
+                console.log("fetch done!");
+                this.issues = response.data.data;
+              },
+              error => {
+                console.log("fetch failed!");
+              }
+            );
         } else {
-          debugger
-          axios.post("/api/v1/issue", req,{
-            headers: { "Authorization": this.AuthStr ,  "Content-Type" : "application/json"}})
-          .then(
-          response => {
-          console.log("Issue created done!");
-          window.location.href = '/issue/'+response.data.data.id
-          //this.issues = response.data.data;
-          },
-            error => {console.log("fetch failed!");});
-          
+          debugger;
+          axios
+            .post("/api/v1/issue", req, {
+              headers: {
+                Authorization: this.AuthStr,
+                "Content-Type": "application/json"
+              }
+            })
+            .then(
+              response => {
+                console.log("Issue created done!");
+                window.location.href = "/issue/" + response.data.data.id;
+                //this.issues = response.data.data;
+              },
+              error => {
+                console.log("fetch failed!");
+              }
+            );
         }
       }
-
-      
     },
     postComment() {
       let req = Object;
@@ -232,17 +283,15 @@ export default {
       req.issue_id = this.id;
 
       axios
-        .post("api/v1/comment/" + this.id, {
-          headers: { Authorization: this.AuthStr },
-          data: {
-            content: req.content,
-            issue_id: req.issue_id
-          }
+        .post("api/v1/comment/" + this.id,req, {
+          headers: { Authorization: this.AuthStr,  "Content-Type": "application/json" }
         })
         .then(
           response => {
             console.log("comment posted!");
-            this.comments.push(req);
+            debugger
+            response.data.data.user_name = getNamebyId(response.data.data.user_id);
+            this.comments.push(response.data.data);
           },
           error => {
             console.log("comment post failed!");
@@ -261,14 +310,26 @@ export default {
           axios.get("/api/v1/labs", {
             headers: { Authorization: this.AuthStr }
           }),
-          axios.get("/api/users", { headers: { Authorization: this.AuthStr } })
-          // axios.get("/api/v1/comments/{issue_id}", { headers: { Authorization: this.AuthStr }}),
+          axios.get("/api/users", { headers: { Authorization: this.AuthStr } }),
+          axios.get("/api/v1/comment/"+this.id, { headers: { Authorization: this.AuthStr }}),
         ])
         .then(
           axios.spread(
-            (issueResp, labResp) => {
-              this.issue = issueResp.data.data;
+            (issueResp, labResp, userResp, commentResp) => {
               this.labs = labResp.data.data.map(x => x.location);
+              this.assignables = userResp.data.data.map(x => {
+                let t = Object();
+                t.name = x.first_name + " " + x.last_name;
+                t.id = x.id;
+                return t;
+              });
+
+              this.issue = issueResp.data.data;
+              this.issue.assigned_to = this.issue.user_id;
+
+              commentResp.data.data=  commentResp.data.data.map( el=> el.user_name = getNamebyId(el.user_id));
+              this.comments=commentResp.data.data;
+
             },
             error => {
               console.log("fetch failed!");
@@ -292,6 +353,7 @@ export default {
                 t.id = x.id;
                 return t;
               });
+
               this.labs = labResp.data.data.map(x => x.location);
             },
             error => {
