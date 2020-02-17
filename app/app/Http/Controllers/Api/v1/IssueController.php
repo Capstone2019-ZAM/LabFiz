b11a82ac4a6a63dfb9987e035f11d1c8d4f5f20a
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-02-16 22:15:40 -0600

    This has issues with create() added TODO comments

--- app/app/Http/Controllers/Api/v1/IssueController.php
+++ app/app/Http/Controllers/Api/v1/IssueController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers\api\v1;
 use App\Contracts\RestServiceContract;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Issue\CreateRequest;
+//Remove these imports once user is identifiable
+use App\Repositories\ModelRepository;
+use App\User;
+
 
 class IssueController extends Controller
 {
-    protected $issue_service;
+    protected $issue_service, $model_user;
 
-    public function __construct(RestServiceContract $service)
+    public function __construct(RestServiceContract $service , User $user)
     {
         $this->issue_service = $service;
+        //TODO remove this var
+        $this->model_user = $user;
     }
 
     public function get($id)
@@ -27,10 +33,12 @@ class IssueController extends Controller
         return response($res['response'], $res['status']);
     }
 
-    public function create(CreateRequest $request)
+    public function create(CreateRequest $request, ModelRepository $user)
     {
         $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => []];
         $header = $request->header('Authorization');
+        //$user = DB::table('users')->where('api_token', $header)->first();
+        // TODO : Resolve issue getByColumn 
         $user = $this->model_user->getByColumn($header, 'api_token');
 
         try {
@@ -43,7 +51,7 @@ class IssueController extends Controller
                     'status' => 'Open',
                     'due_date'=> $request->due_date,
                     'description' => $request->description,
-                    'comments' => $request->comments
+                   // 'comments' => $request->comments  // not required
                 ]
             );
         } catch (Exception $ex) {
