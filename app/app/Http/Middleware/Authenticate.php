f53f467275ffa00cecba8c8af90404016ec04d92
Transvestor <itskaian@hotmail.com>
2020-02-18 21:02:51 -0600

    update auth exec handler
    check for empty guards

--- app/app/Http/Middleware/Authenticate.php
+++ app/app/Http/Middleware/Authenticate.php
@@ -11,12 +11,12 @@ class Authenticate extends Middleware
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return string|null
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
+        if (!$request->expectsJson()) {
             return route('login');
         }
     }
@@ -31,17 +31,15 @@ class Authenticate extends Middleware
      */
     public function handle($request, Closure $next, ...$guards)
     {
-        if($guards[0] === 'api' && Auth::guard($guards[0])->user() && Auth::guard($guards[0])->user()->api_token_expiry_date < now()){
-            return response()->json([
-                'status' => '401 (Unauthorized)',
-                'message' => 'Your authentication token has expired, you will need to refresh it using the /refresh route.',
-                'data' => '',
-            ], 401);
-        }
-
-        if (Auth::guard($guards[0])->guest()) {
-            if ($guards[0] !== 'api')
-                return redirect()->guest('login');
+        if (!empty($guards) && $guards[0] === 'api') {
+            $user = Auth::guard('api')->user();
+            if ($user && $user->api_token_expiry_date < now()) {
+                return response()->json([
+                    'status' => '401 (Unauthorized)',
+                    'message' => 'Your authentication token has expired, you will need to refresh it using the /refresh route.',
+                    'data' => '',
+                ], 401);
+            }
         }
 
         return $next($request);
