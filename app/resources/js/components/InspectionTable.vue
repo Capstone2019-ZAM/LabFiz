d4fa49add715212acfe8cf19302f128c4242086c
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-02-23 14:01:25 -0600

    Update InspectionTable.vue

--- app/resources/js/components/InspectionTable.vue
+++ app/resources/js/components/InspectionTable.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center">
-    <v-col cols="12" md="9">
+    <v-col cols="12" md="12" lg="9">
       <v-card>
         <v-card-title>
           All Inspections
@@ -13,13 +13,18 @@
             hide-details
           ></v-text-field>
         </v-card-title>
-        <v-data-table :headers="headers" :items="reports" :search="search">
+        <v-data-table
+          :headers="headers"
+          :items="reports"
+          :search="search"
+          :loading="loading"
+          loading-text="Loading... Please wait"
+        >
           <template v-slot:item.status="{ item }">
             <v-chip :color="getColor(item.status)" dark>{{ item.status }}</v-chip>
-          
           </template>
 
-          <v-card>
+          <!-- <v-card>
             <v-card-title>
               <span class="headline">Edit Assignment</span>
             </v-card-title>
@@ -28,7 +33,7 @@
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.status" label="Fat (g)"></v-text-field>
+                    <v-text-field v-model="editedItem.status" label=""></v-text-field>
                   </v-col>
                 </v-row>
               </v-container>
@@ -39,12 +44,15 @@
               <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
               <v-btn color="blue darken-1" text @click="save">Save</v-btn>
             </v-card-actions>
-          </v-card>
+          </v-card>-->
 
           <!-- <v-dialog v-model="dialog" max-width="500px"> -->
+          <template v-slot:item.title="{ item }">
+            <a class="nav" :href="viewLink(item)">{{item.title}}</a>
+          </template>
 
           <template v-slot:item.action="{ item }">
-            <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+            <!-- <v-icon small class="mr-2" @click="editItem(item)">mdi-pencil</v-icon> -->
             <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
           </template>
 
@@ -61,6 +69,7 @@ export default {
   data() {
     return {
       search: "",
+      loading: false,
       AuthStr: localStorage.getItem("api"),
       dialog: false,
       users: null,
@@ -74,29 +83,27 @@ export default {
         },
         { text: "Status", value: "status", width: "100px" },
 
-        { text: "Report", value: "title", width: "200px" },
+        { text: "Report", value: "title", width: "150px" },
         { text: "Assigned To", value: "user_name", width: "150px" },
         { text: "Due Date", value: "due_date", width: "100px" },
         { text: "Actions", value: "action", sortable: false, width: "50px" }
       ],
-      editedIndex: -1,
-      editedItem: {
-        report: "",
-        room: "",
-        status: "",
-        assigned_to: ""
-      },
       defaultItem: {
-        report: "",
-        room: "",
-        status: "",
-        assigned_to: ""
+        // report: "",
+        // room: "",
+        // status: "",
+        // assigned_to: ""
       },
-      created() {
-        this.initialize();
-      },
+      temp_reports: [],
       reports: [
-    
+        // {
+        //   status: "Pending",
+        //   lab: "ED-401",
+        //   user_name: "John Doe",
+        //   user_id: 1,
+        //   title: "Wet Lab",
+        //   due_date: "14-Jan-2020"
+        // }
       ]
     };
   },
@@ -105,11 +112,18 @@ export default {
       val || this.close();
     }
   },
-
+  computed: {},
   created() {
     this.initialize();
   },
   methods: {
+    getNamebyId(t_id) {
+      let n = this.users.find(x => x.id == t_id);
+      return n.first_name + " " + n.last_name;
+    },
+    viewLink(i) {
+      return "/report/" + i.id;
+    },
     getColor(status) {
       if (status == "Pending") return "blue";
       else if (status == "Submitted") return "green";
@@ -117,88 +131,66 @@ export default {
       else return "grey";
     },
     initialize() {
-
-      axios
-      .get("/api/v1/reports" , {
-        headers: { Authorization:'Bearer '+ this.AuthStr }
-      })
-      .then(
-        response => {
-          console.log("reports fetch done!");
-          this.reports = response.data.data
-          
-        },
-        error => {
-          console.log("reports fetch failed!");
-          this.loading = false;
-          this.NetError = true;
-        }
-      );
-            axios
-      .get("/api/users" , {
-        headers: { Authorization:'Bearer '+ this.AuthStr }
-      })
-      .then(
-        response => {
-          console.log("user fetch done!");
-          this.users = response.data.data
-          debugger
-        },
-        error => {
-          console.log("user fetch failed!");
-          this.loading = false;
-          this.NetError = true;
-        }
-      );
-
-      // this.reports = [
-      //       {
-      //     status: "Pending",
-      //     lab: "ED-401",
-      //     user_name: "John Doe",
-      //     title: "Wet Lab",
-      //     due_date: "14-Jan-2020"
-      //   },
-      //   {
-      //     status: "Review Required",
-      //     lab: "ED-401",
-      //     user_name: "John Doe",
-      //     title: "Wet Lab",
-      //     due_date: "14-Jan-2020"
-      //   },
-      //   {
-      //     status: "Overdue",
-      //     lab: "ED-401",
-      //     user_name: "Johnathan Doe",
-      //     title: "Wet Lab",
-      //     due_date: "14-Jan-2020"
-      //   },
-      //   {
-      //     status: "Pending",
-      //     lab: "ED-401",
-      //     user_name: "John Doe",
-      //     title: "Wet Lab",
-      //     due_date: "14-Jan-2020"
-      //   },
-      //   {
-      //     status: "Submitted",
-      //     lab: "ED-310",
-      //     user_name: "John Doe",
-      //     title: "Wet Lab",
-      //     due_date: "10-Jul-2020"
-      //   }
-      // ];
+      (this.loading = true),
+        axios
+          .get("/api/v1/reports", {
+            headers: { Authorization: "Bearer " + this.AuthStr }
+          })
+          .then(
+            response => {
+              console.log("reports fetch done!");
+              this.temp_reports = response.data.data;
+              this.loading = false;
+              return axios.get("/api/users", {
+                headers: { Authorization: "Bearer " + this.AuthStr }
+              });
+            },
+            error => {
+              this.loading = false;
+              console.log("reports fetch failed!");
+            }
+          )
+          .then(
+            response => {
+              console.log("user fetch done!");
+              this.users = response.data.data;
+              this.temp_reports.map(rpt => {
+                rpt.user_name = this.getNamebyId(rpt.assigned_to);
+              });
+              this.reports = this.temp_reports;
+              this.loading = false;
+            },
+            error => {
+              console.log("user fetch failed!");
+              this.loading = false;
+            }
+          );
     },
     editItem(item) {
-      this.editedIndex = this.reports.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
+      // this.editedIndex = this.reports.indexOf(item);
+      // this.editedItem = Object.assign({}, item);
+      // this.dialog = true;
     },
 
     deleteItem(item) {
       const index = this.reports.indexOf(item);
+      this.loading = true;
       confirm("Are you sure you want to delete this item?") &&
-        this.reports.splice(index, 1);
+        axios
+          .delete("/api/v1/report/" + item.id, {
+            headers: { Authorization: "Bearer " + this.AuthStr }
+          })
+          .then(
+            response => {
+              console.log("Inspection instance deleted!");
+              this.reports.splice(index, 1);
+              this.loading = false;
+            },
+            error => {
+              console.log("Inspection delete failed!");
+              this.loading = false;
+            }
+          );
     },
 
     close() {
