7bf0fd0b71d3bf5a3d4084563720939d8194c406
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-03-01 21:09:45 -0600

    added on initialize functionality

--- app/resources/js/components/Assignment.vue
+++ app/resources/js/components/Assignment.vue
@@ -1,17 +1,46 @@
 <template>
-<div>
-    <assignment-header></assignment-header>
+  <div>
+    <assignment-header :data="this.section_header"></assignment-header>
     <assignment-section></assignment-section>
-</div>
+  </div>
 </template>
 
 <script>
-import AssignmentHeader from './AssignmentHeader'
-import AssignmentSection from './AssignmentSection'
+import AssignmentHeader from "./AssignmentHeader";
+import AssignmentSection from "./AssignmentSection";
 export default {
-    components: {
-        'assignment-header': AssignmentHeader,
-        'assignment-section' :AssignmentSection
+  components: {
+    "assignment-header": AssignmentHeader,
+    "assignment-section": AssignmentSection
+  },
+  data() {
+    return {
+      id: parseInt(window.location.pathname.split("/").pop()), //get this dynamic or from url
+      AuthStr: "Bearer " + localStorage.getItem("api"),
+      section_header: null,
+    };
+  },
+  created() {
+    this.initialize();
+  },
+  methods: {
+    initialize() {
+      if (Number.isInteger(this.id)) {
+        axios
+          .get("/api/v1/report/" + this.id, {
+            headers: { Authorization: this.AuthStr }
+          })
+          .then(
+            response => {
+              console.log("fetch done!");
+              this.section_header = response.data.data;
+            },
+            error => {
+              console.log("fetch failed!");
+            }
+          );
+      }
     }
-}
+  }
+};
 </script>
