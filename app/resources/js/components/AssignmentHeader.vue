9b2708dd8cfbc1b96eb4e46b8f23eb5a74a0bb6f
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-03-01 21:10:09 -0600

    Added props

--- app/resources/js/components/AssignmentHeader.vue
+++ app/resources/js/components/AssignmentHeader.vue
@@ -1,14 +1,17 @@
 <template>
+
   <v-row justify="center">
-    <v-card width="80%" max-width="1000px">
+    <v-card width="80%" max-width="1000px"
+     v-if="data !== null" >
       <v-row>
-        <v-col cols="12" md="9">
-          <v-card-title>Wet Lab - Inspection</v-card-title>
+        <v-col cols="12" md="8">
+          <v-card-title>{{data.title}}</v-card-title>
        
-          <v-card-subtitle class="headline ">ED-440</v-card-subtitle>
-          <v-card-text class="title">Due Date : 12-Jan-2010</v-card-text>
+          <v-card-subtitle class="headline">{{data.lab}}</v-card-subtitle>
         </v-col>
-        <v-col cols="12" md="3">
+        <v-col cols="12" md="4">
+          <v-card-subtitle class="subtitle-1	">{{'Status: ' +data.status}}<br>          
+          {{'Due Date: ' +data.due_date}}</v-card-subtitle>
         <v-card-actions >
           <v-btn color="primary" dark>Save</v-btn>
           <v-btn color="primary">Submit</v-btn>
@@ -21,5 +24,7 @@
 
 
 <script>
-export default {};
+export default {
+    props: ["data"],
+};
 </script>
