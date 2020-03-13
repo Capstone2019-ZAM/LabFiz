6ad16998b04177834fa82e8fcb6d47b3cb2b3c11
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-03-12 20:54:09 -0600

    Added route guards

--- app/routes/web.php
+++ app/routes/web.php
@@ -22,62 +22,73 @@ Route::get('/', function () {
 Auth::routes();
 
 // generic routes
-Route::group(['middleware' => ['role:admin|instructor|student']], function(){
-    Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['role:admin|inspector|student']], function(){
+   // Route::get('/home', 'HomeController@index')->name('home');
+   
+    Route::get('/home', function () {
+        return view('dashboardPage');
+    });
+    Route::get('/dashboard', function () {
+        return view('dashboardPage');
+    });
+
 });
 
 // admin routes
 Route::group(['middleware' => ['role:admin']], function(){
-    Route::get('/admin','AdminController@index')->name('index');
+    //Route::get('/admin','AdminController@index')->name('index');
+    Route::get('/templates', function () {
+        return view('template_rpt');
+    });
+    // Route::get('/template/{id}', function () {
+    //     return view('template_builder');
+    // });
+    
+    Route::get('/template/{id}', function () {
+        return view('template_builder');
+    });
+    Route::get('/template', function () {
+        return view('template_builder');
+    });
+    Route::get('/account', function () {
+        return view('account_rpt');
+    });
+    Route::get('/account/{id}', function () {
+        return view('account_rpt');
+    });
+    Route::get('/accounts', function () {
+        return view('account');
+    });
+    Route::get('/restore', function () {
+        return view('restore');
+    });
+    Route::get('/assignments', function () {
+        return view('assignment_rpt');
+    });
 });
 
 
-Route::get('/dashboard', function () {
-    return view('dashboardPage');
+Route::group(['middleware' => ['role:admin|inspector']], function(){
+
+    Route::get('/myassignments', function () {
+        return view('assignment_my_rpt');
+    });
+    Route::get('/assignment/{id}', function () {
+        return view('assignment');
+    });
+    Route::get('/issue/{id}', function () {
+        return view('issue_rpt');
+    });
+    Route::get('/issue', function () {
+        return view('issue_rpt');
+    });
+    Route::get('/issues', function () {
+        return view('issue');
+    });
+    
 });
 
 
 
-Route::get('/templates', function () {
-    return view('template_rpt');
-});
-// Route::get('/template/{id}', function () {
-//     return view('template_builder');
-// });
 
-Route::get('/template/{id}', function () {
-    return view('template_builder');
-});
-Route::get('/template', function () {
-    return view('template_builder');
-});
-Route::get('/assignments', function () {
-    return view('assignment_rpt');
-});
-Route::get('/myassignments', function () {
-    return view('assignment_my_rpt');
-});
-Route::get('/assignment/{id}', function () {
-    return view('assignment');
-});
-Route::get('/issue/{id}', function () {
-    return view('issue_rpt');
-});
-Route::get('/issue', function () {
-    return view('issue_rpt');
-});
-Route::get('/issues', function () {
-    return view('issue');
-});
-Route::get('/account', function () {
-    return view('account_rpt');
-});
-Route::get('/account/{id}', function () {
-    return view('account_rpt');
-});
-Route::get('/accounts', function () {
-    return view('account');
-});
-Route::get('/restore', function () {
-    return view('restore');
-});
+
