402b8650f7693728e06e38cbea2e5c14d9a638d6
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-03-12 21:21:09 -0600

    Implement low level access control for dash buttons

--- app/app/Http/Controllers/Api/v1/DashboardController.php
+++ app/app/Http/Controllers/Api/v1/DashboardController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Dashboard;
 use App\Repositories\ModelRepository;
 use App\User;
+use Illuminate\Support\Facades\Auth;
+
 use Exception;
 
 
@@ -28,8 +30,17 @@ class DashboardController extends Controller
      */
     public function get_all()
     {
+        $user = Auth::guard('api')->user();
+        $role =  $user->getRoleNames();
+        //dd($role[0]);
         $result = ['status' => '400 (Bad Request)', 'message' => '', 'data' => ''];
-        $result['data'] = $this->model_dashboard->get();
+        if ($role[0]=="admin" ){
+            $result['data'] = $this->model_dashboard->get();
+        }
+        else{
+            $result['data'] = $this->model_dashboard->where('accessible_to',2)->get();
+
+        }
         $result['status'] = '200 (Ok)';
         $result['message'] = 'All Dashboard options retrieved succesfully.';
         return response($result, 200);
