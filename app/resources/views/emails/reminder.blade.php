f29e4b9ee0267aaba473eb4b31aa28658c2a2f49
Muhammad Ahmed <muhammadmanzoor16@gmail.com>
2020-03-28 00:02:13 -0600

    Added reminder task schedule

---
+++ app/resources/views/emails/reminder.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inspection Reminder</title>
+    <style>
+        section {
+            font-family: sans-serif;
+            font-size: medium;
+            
+        }
+
+        #wrapper {
+            background: #26a69a;
+            margin: 10px;
+            padding: 10px;
+            display: flex;
+            flex-direction: column;
+            align-content: center;
+            justify-content: center;
+            justify-items: center;
+            max-width: fit-content;
+
+
+        }
+
+
+        #content {
+            background: #f8fafa;
+            padding: 10px;
+            max-width: fit-content;
+            display: flex;
+            align-self: center;
+
+        }
+    </style>
+
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="content">
+            <section>
+                <div id="msg">
+                    <p>
+                    <h3 id="title">Inspection Reminder</h3>
+                    </p>
+                    <p> Hey {{$name}}, </p>
+                    <p> You have outstanding inpsections that are due soon. Please complete them at
+                         <a href="www.labfiz.com/login"> www.labfiz.com</a>.
+                    </p>                   
+                    <p>
+                        Regards,
+                    <br>
+                        LabFiz Admin
+                    </p>
+                </div>
+            </section>
+        </div>
+    </div>
+</body>
+
+</html>
